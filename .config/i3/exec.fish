f5f1c8ce7d645d9174797fa7c70006158219fe3e
Šimon Brandner <simon@Simon-s-PC.Home>
2020-07-21 17:36:31 +0200

    2020-07-21-17:36:30

--- .config/i3/exec.fish
+++ .config/i3/exec.fish
@@ -5,7 +5,7 @@ latte-dock & # Start latte-dock
 bash /home/simon/.config/conky/conky.sh & # Start Conky
 sleep 5
 megasync & # Start MEGASync
-ulauncher & # Start MEGASync
+#ulauncher & # Start MEGASync
 
 rm ~/core # Remove the core file
 #barrier" # Start Barrier
@@ -16,9 +16,9 @@ i3-msg 'workspace 0; append_layout /home/simon/.config/i3/workspace-0.json' & # 
 /opt/viber/Viber %u & # Start Viber
 spotify & # Start Spotify
 discord & # Start Discord
-whatsdesk --no-sandbox & # Start Whatsdesk
+#whatsdesk --no-sandbox & # Start Whatsdesk
 bash -lic 'LD_PRELOAD=/home/simon/GIT/NotMine/android-messageswm/android-messageswm /home/simon/PortableApps/AndroidMessages/android-messages' & # Start Android Messages
 /home/simon/PortableApps/nativefier-apps/todoist-linux-x64/todoist --no-sandbox & # Start Todoist
-teams & # Start MS Teams
+#teams & # Start MS Teams
 evolution & # Start Evolution
 # Workspace 0  
