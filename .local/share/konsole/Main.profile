e5e4252a57e9354c6605c7187b954892d5a05e9a
Šimon Brandner <simon.bra.ag@gmail.com>
2020-11-13 13:26:47 +0100

    11/13/20-13:26:47

--- .local/share/konsole/Main.profile
+++ .local/share/konsole/Main.profile
@@ -1,6 +1,7 @@
 [Appearance]
 ColorScheme=One Dark
 Font=MesloLGS Nerd Font,13,-1,5,50,0,0,0,0,0
+LineSpacing=0
 
 [Cursor Options]
 CursorShape=1
@@ -9,9 +10,19 @@ CursorShape=1
 Command=
 Name=Main
 Parent=FALLBACK/
+ShowTerminalSizeHint=false
+TerminalCenter=true
+TerminalMargin=3
+
+[Interaction Options]
+UnderlineFilesEnabled=true
 
 [Scrolling]
+HighlightScrolledLines=false
+HistoryMode=2
 ScrollBarPosition=2
+ScrollFullPage=1
 
 [Terminal Features]
 BlinkingCursorEnabled=true
+UrlHintsModifiers=0
--- scripts/exec.zsh
+++ scripts/exec.zsh
@@ -7,8 +7,8 @@
 ############################################################
 #latte-dock & # Start LatteDock
 #barrier" # Start Barrier
-picom --experimental-backends # Start Picom
-bash /home/simon/.config/conky/conky.sh & # Start Conky
+picom --experimental-backends & # Start Picom
+#bash /home/simon/.config/conky/conky.sh & # Start Conky
 sleep 5
 megasync & # Start MEGASync
 variety -n & # Start Variety
@@ -28,15 +28,9 @@ i3-msg 'workspace 0; append_layout /home/simon/GIT/Other/dotfiles/i3Layouts/work
 #bash -lic 'LD_PRELOAD=/home/simon/GIT/NotMine/android-messageswm/android-messageswm /home/simon/PortableApps/AndroidMessages/android-messages' & # Start Android Messages
 #whatsdesk --no-sandbox & # Start Whatsdesk
 
-# Wait for PulseAudio
-#while ! pgrep pulseaudio; do sleep 1; done
-#sleep 1
-# Wait for PulseAudio
-
 /home/simon/PortableApps/nativefier-apps/todoist-linux-x64/todoist --no-sandbox & # Start Todoist
 evolution & # Start Evolution
-
-spotify &
+spotify & # Start Spotify
 /opt/viber/Viber & # Start Viber
 discord & # Start Discord
 teams & # Start MS Teams
